ff2b9ce82c37f71b16de0cfcb52d1709117ceacb
wfilho91 <wellington79620@gmail.com>
2021-01-04 19:21:07 -0300

    GET/orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -18,9 +18,6 @@ import javax.persistence.Table;
 @Table(name= "tb_order")
 public class Order implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
