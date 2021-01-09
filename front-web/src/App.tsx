f513894351d69f92e94016197cbffcbc1dfb976d
wfilho91 <wellington79620@gmail.com>
2021-01-09 00:02:06 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
-import Navbar from './Navbar'
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div >
       <Navbar />
+      <Home/>
     </div>
   );
 }
