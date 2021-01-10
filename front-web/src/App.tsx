e2d82f46fec2bb977172e05bc1a5d9e32c06bba6
wfilho91 <wellington79620@gmail.com>
2021-01-09 21:21:58 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './Home';
 import Navbar from './Navbar';
-
+import Footer from './Footer';
 function App() {
   return (
     <div >
       <Navbar />
       <Home/>
+      <Footer/>
     </div>
   );
 }
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import {ReactComponent as Logo} from './logo.svg'
+
+function Orders(){
+
+    return(
+       <div>
+           <h1></h1>
+       </div>
+    )
+
+}
+
+export default Orders;
